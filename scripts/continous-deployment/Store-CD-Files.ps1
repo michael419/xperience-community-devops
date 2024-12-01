4e0fad27fa695dbe5051812efef0aed751ca4f07
Michael Eustace <michael.eustace@moderncitizens.com>
2024-12-01 20:41:01 +0000

    fix: Fixed syntax for zip compression powershell command

--- scripts/continous-deployment/Store-CD-Files.ps1
+++ scripts/continous-deployment/Store-CD-Files.ps1
@@ -6,6 +6,6 @@ Write-Output "Creating CDRepository files..."
 dotnet run --no-build -- --kxp-cd-store --repository-path "$PSScriptRoot\App_Data\CDRepository"
 
 # If your build server runs on Windows Server 2012 or older, uncomment the below line to create a zip of the CD files to avoid hitting the 255 file path max character length issue 
-# Compress-Archive -Path "$PSScriptRoot\App_Data\CDRepository\@global", "$PSScriptRoot\App_Data\CDRepository\repository.config" -DestinationPath "$PSScriptRoot\App_Dat
+# Compress-Archive -Path "$PSScriptRoot\App_Data\CDRepository\@global", "$PSScriptRoot\App_Data\CDRepository\repository.config" -DestinationPath "$PSScriptRoot\App_Data\CDRepository.zip" -Force
 
 Write-Output "CDRepository files successfully created"
