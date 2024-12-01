69d5531442414c8963c4c251d8322796cfeaca9f
michael419 <michael.eustace@moderncitizens.com>
2024-12-01 19:47:05 +0000

    feat: Added continuous deployment powershell script for use in a deployment pipeline to restore objects in a hosted XbyK website from the CD repository

---
+++ scripts/continous-deployment/Restore-CD-Files.ps1
@@ -0,0 +1,23 @@
+# Used to restore objects in your hosted Xperience by Kentico website
+# Use this file in your deployment pipeline after you have deployed your 
+
+$TargetLocation = '<PATH_TO_WEBSITE_ROOT_DIRECTORY>'
+$XbyKWebsiteDllFileName = '<YOUR_XBYK_WEBSITE_ASSEMBLY>.dll'
+$CDRepositoryLocation = "$TargetLocation\App_Data\CDRepository"
+$lockDirName = ".lock"
+
+Write-Output "Deleting CDRepository directory contents..."
+Get-ChildItem -Path $CDRepositoryLocation -Recurse | Where-Object {
+    $_.FullName -notlike "*\$lockDirName*"
+} | Remove-Item -Recurse -Force
+Write-Output "CDRepository directory contents successfully deleted"
+
+# If you zipped up the CD files in conjunction with the Store-CD-Files powershell script, ensure you comment the below 3 lines
+# Write-Output "Unzipping CDRepository.zip..."
+# Expand-Archive -Path "$TargetLocation\App_Data\CDRepository.zip" -DestinationPath "$CDRepositoryLocation"
+# Write-Output "CDRepository.zip successfully extracted"
+
+Write-Output "Restoring CD repository..."
+Set-Location -Path $TargetLocation
+dotnet $XbyKWebsiteDllFileName --kxp-cd-restore --repository-path "$CDRepositoryLocation"
+Write-Output "Restoring CD repository..."
